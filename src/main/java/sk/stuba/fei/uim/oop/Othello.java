c9612a276031fed67481c140337b176d48c0f0d4
Jakub <xchrappaj@stuba.sk>
2022-03-28 08:55:30 +0200

    Settings

--- src/main/java/sk/stuba/fei/uim/oop/Othello.java
+++ src/main/java/sk/stuba/fei/uim/oop/Othello.java
@@ -1,12 +1,17 @@
 package sk.stuba.fei.uim.oop;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Othello {
 
     public Othello(){
 
         JFrame okno = new JFrame("Hra Othello");
+        okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        okno.setSize(550,550);
+        okno.setResizable(false);
+        okno.setLayout(new GridLayout());
 
 
         okno.setVisible(true);
---
+++ src/main/java/sk/stuba/fei/uim/oop/OthelloNastavenia.java
@@ -0,0 +1,4 @@
+package sk.stuba.fei.uim.oop;
+
+public class OthelloNastavenia extends ZakladneNastavenia{
+}

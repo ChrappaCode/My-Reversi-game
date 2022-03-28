4c132d10a78c505a62f25b98f702d4bdfec24d2a
Jakub <xchrappaj@stuba.sk>
2022-03-28 08:25:50 +0200

    Jak sa hovori skoro hotové (vypis okna)

---
+++ src/main/java/sk/stuba/fei/uim/oop/Othello.java
@@ -0,0 +1,14 @@
+package sk.stuba.fei.uim.oop;
+
+import javax.swing.*;
+
+public class Othello {
+
+    public Othello(){
+
+        JFrame okno = new JFrame("Hra Othello");
+
+
+        okno.setVisible(true);
+    }
+}
